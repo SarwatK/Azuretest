bf263c6cd87ddee75b3d0a1d4b6154e54bd8bb55
SarwatK <sarwat.kadri@capgemini.com>
2024-09-20 21:41:38 +0530

    vnet4

--- Vnet/Vnet.tf
+++ Vnet/Vnet.tf
@@ -1,8 +1,5 @@
 provider "azurerm" {
-
   features {}
-  
-  
 }
 # Define the resource group
 resource "azurerm_resource_group" "example" {
@@ -19,12 +16,11 @@ resource "azurerm_resource_group" "new" {
 # Define the virtual network
 resource "azurerm_virtual_network" "new" {
   name                = "new-vnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.20.0.0/16"]
   location            = azurerm_resource_group.new.location
   resource_group_name = azurerm_resource_group.new.name
 
   tags = {
-    
   }
 }
 # Define a subnet within the virtual network
@@ -32,7 +28,7 @@ resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1-subnet"
   resource_group_name  = azurerm_resource_group.new.name
   virtual_network_name = azurerm_virtual_network.new.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.20.1.0/24"]
 
   service_endpoints = [
     "Microsoft.Storage",
